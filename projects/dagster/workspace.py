627e91a52bfe01ab4899f48ab281514335e335c9
Victor Bart <victorbart@MacBook-Pro-de-Victor-2.local>
2023-10-10 14:23:06 +0200

    Fix python dependencies problems

--- projects/dagster/workspace.py
+++ projects/dagster/workspace.py
@@ -1,11 +1,11 @@
-from dagster import job, repository
-from dagster_dbt import dbt_cli_resource
-from dagster_pyspark import pyspark_resource
-
 from db import initialize_db, drop_tables_op, create_schemas_op, trino_resource
 from download import download_yahoo_finance_files, download_yahoo_finance_files_op
-from dbt import dbt_bronze, dbt_silver, dbt_gold, dbt_all, dbt_bronze_run_op, dbt_silver_run_op, dbt_gold_run_op, dbt_bronze_test_doc_sources_op, dbt_silver_test_doc_sources_op, dbt_gold_test_doc_sources_op
+from dbt_init import dbt_bronze, dbt_silver, dbt_gold, dbt_all, dbt_bronze_run_op, dbt_silver_run_op, dbt_gold_run_op, dbt_bronze_test_doc_sources_op, dbt_silver_test_doc_sources_op, dbt_gold_test_doc_sources_op
 from predict import predict_op
+
+from dagster import job, repository
+from dagster_pyspark import pyspark_resource
+from dagster_dbt import dbt_cli_resource
 
 @job(resource_defs={'dbt': dbt_cli_resource, 'trino': trino_resource, 'pyspark': pyspark_resource})
 def e2e():    
